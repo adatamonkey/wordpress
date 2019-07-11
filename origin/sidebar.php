133b2d5658cc8b065e561149f1b529d991f227c0
adatamonkey <adatamonkey@gmail.com>
2019-07-12 00:02:31 +0900

    index.phpを分解

---
+++ origin/sidebar.php
@@ -0,0 +1,23 @@
+<div>
+  <div class="widget">
+    <h2>Category</h2>
+    <ul>
+      <li><a href="#">カテゴリ名</a></li>
+      <li><a href="#">カテゴリ名</a></li>
+    </ul>
+  </div>
+  <div class="widget">
+    <h2>Tag</h2>
+    <div class="tagcloud">
+      <a class="tag-cloud-link" href="#">タグ</a>
+      <a class="tag-cloud-link" href="#">タグ</a>
+    </div>
+  </div>
+  <div class="widget">
+    <h2>Archive</h2>
+    <ul>
+      <li><a href="#">2017年10月</a></li>
+      <li><a href="#">2017年9月</a></li>
+    </ul>
+  </div>
+</div>
